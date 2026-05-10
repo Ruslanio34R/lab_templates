66ec8687b93cd6489099ff0ac1cd13dd1d0c776c
Ruslanio34R <eurosetizberg007@gmail.com>
2026-05-10 17:33:39 +0300

    unique_ptr.h

--- unique_ptr.h
+++ unique_ptr.h
@@ -3,40 +3,30 @@
 #include <cstddef>
 #include <memory>
 #include <utility>
-#include <type_traits>
+#include <tuple>
 
 template <typename T, typename Deleter = std::default_delete<T>>
 class UniquePtr {
 public:
     // ======================== Constructors ========================
-    UniquePtr() noexcept : ptr_(nullptr) {}
-    explicit UniquePtr(T* ptr) noexcept : ptr_(ptr) {}
-    UniquePtr(T* ptr, const Deleter& deleter) 
-        : ptr_(ptr) {
-        new (&get_deleter()) Deleter(deleter);
-    }
-    UniquePtr(T* ptr, Deleter&& deleter) 
-        : ptr_(ptr) {
-        new (&get_deleter()) Deleter(std::move(deleter));
-    }
+    UniquePtr() noexcept : tuple_(nullptr, Deleter()) {}
+    explicit UniquePtr(T* ptr) noexcept : tuple_(ptr, Deleter()) {}
+    UniquePtr(T* ptr, const Deleter& deleter) : tuple_(ptr, deleter) {}
+    UniquePtr(T* ptr, Deleter&& deleter) : tuple_(ptr, std::move(deleter)) {}
 
     // =================== No copy ==================================
     UniquePtr(const UniquePtr&) = delete;
     UniquePtr& operator=(const UniquePtr&) = delete;
 
     // =================== Move ====================================
-    UniquePtr(UniquePtr&& other) noexcept
-        : ptr_(other.ptr_) {
-        new (&get_deleter()) Deleter(std::move(other.get_deleter()));
-        other.ptr_ = nullptr;
-    }
+    UniquePtr(UniquePtr&& other) noexcept 
+        : tuple_(other.release(), std::move(other.get_deleter())) {}
     
     UniquePtr& operator=(UniquePtr&& other) noexcept {
         if (this != &other) {
             reset();
-            ptr_ = other.ptr_;
+            std::get<0>(tuple_) = other.release();
             get_deleter() = std::move(other.get_deleter());
-            other.ptr_ = nullptr;
         }
         return *this;
     }
@@ -44,50 +34,42 @@ public:
     // =================== Destructor ==============================
     ~UniquePtr() {
         reset();
-        get_deleter().~Deleter();
     }
 
     // =================== Observers ===============================
-    T* get() noexcept { return ptr_; }
-    const T* get() const noexcept { return ptr_; }
-    explicit operator bool() const noexcept { return ptr_ != nullptr; }
+    T* get() noexcept { return std::get<0>(tuple_); }
+    const T* get() const noexcept { return std::get<0>(tuple_); }
+    explicit operator bool() const noexcept { return get() != nullptr; }
 
-    T& operator*() noexcept { return *ptr_; }
-    const T& operator*() const noexcept { return *ptr_; }
+    T& operator*() noexcept { return *get(); }
+    const T& operator*() const noexcept { return *get(); }
 
-    T* operator->() noexcept { return ptr_; }
-    const T* operator->() const noexcept { return ptr_; }
+    T* operator->() noexcept { return get(); }
+    const T* operator->() const noexcept { return get(); }
 
-    Deleter& get_deleter() noexcept {
-        return reinterpret_cast<Deleter&>(storage_);
-    }
-    
-    const Deleter& get_deleter() const noexcept {
-        return reinterpret_cast<const Deleter&>(storage_);
-    }
+    Deleter& get_deleter() noexcept { return std::get<1>(tuple_); }
+    const Deleter& get_deleter() const noexcept { return std::get<1>(tuple_); }
 
     // =================== Modifiers ===============================
     T* release() noexcept {
-        T* tmp = ptr_;
-        ptr_ = nullptr;
+        T* tmp = get();
+        std::get<0>(tuple_) = nullptr;
         return tmp;
     }
     
     void reset(T* ptr = nullptr) noexcept {
-        if (ptr_) {
-            get_deleter()(ptr_);
+        if (get()) {
+            get_deleter()(get());
         }
-        ptr_ = ptr;
+        std::get<0>(tuple_) = ptr;
     }
     
     void swap(UniquePtr& other) noexcept {
-        std::swap(ptr_, other.ptr_);
-        std::swap(get_deleter(), other.get_deleter());
+        std::swap(tuple_, other.tuple_);
     }
 
 private:
-    T* ptr_;
-    mutable typename std::aligned_storage<sizeof(Deleter), alignof(Deleter)>::type storage_;
+    std::tuple<T*, Deleter> tuple_;
 };
 
 // =====================================================================
@@ -98,16 +80,10 @@ template <typename T, typename Deleter>
 class UniquePtr<T[], Deleter> {
 public:
     // ======================== Constructors ========================
-    UniquePtr() noexcept : ptr_(nullptr) {}
-    explicit UniquePtr(T* ptr) noexcept : ptr_(ptr) {}
-    UniquePtr(T* ptr, const Deleter& deleter) 
-        : ptr_(ptr) {
-        new (&get_deleter()) Deleter(deleter);
-    }
-    UniquePtr(T* ptr, Deleter&& deleter) 
-        : ptr_(ptr) {
-        new (&get_deleter()) Deleter(std::move(deleter));
-    }
+    UniquePtr() noexcept : tuple_(nullptr, Deleter()) {}
+    explicit UniquePtr(T* ptr) noexcept : tuple_(ptr, Deleter()) {}
+    UniquePtr(T* ptr, const Deleter& deleter) : tuple_(ptr, deleter) {}
+    UniquePtr(T* ptr, Deleter&& deleter) : tuple_(ptr, std::move(deleter)) {}
 
     // =================== No copy ==================================
     UniquePtr(const UniquePtr&) = delete;
@@ -115,17 +91,13 @@ public:
 
     // =================== Move ====================================
     UniquePtr(UniquePtr&& other) noexcept
-        : ptr_(other.ptr_) {
-        new (&get_deleter()) Deleter(std::move(other.get_deleter()));
-        other.ptr_ = nullptr;
-    }
+        : tuple_(other.release(), std::move(other.get_deleter())) {}
     
     UniquePtr& operator=(UniquePtr&& other) noexcept {
         if (this != &other) {
             reset();
-            ptr_ = other.ptr_;
+            std::get<0>(tuple_) = other.release();
             get_deleter() = std::move(other.get_deleter());
-            other.ptr_ = nullptr;
         }
         return *this;
     }
@@ -133,47 +105,39 @@ public:
     // =================== Destructor ==============================
     ~UniquePtr() {
         reset();
-        get_deleter().~Deleter();
     }
 
     // =================== Observers ===============================
-    T* get() noexcept { return ptr_; }
-    const T* get() const noexcept { return ptr_; }
-    explicit operator bool() const noexcept { return ptr_ != nullptr; }
+    T* get() noexcept { return std::get<0>(tuple_); }
+    const T* get() const noexcept { return std::get<0>(tuple_); }
+    explicit operator bool() const noexcept { return get() != nullptr; }
 
-    T& operator[](size_t index) noexcept { return ptr_[index]; }
-    const T& operator[](size_t index) const noexcept { return ptr_[index]; }
+    T& operator[](size_t index) noexcept { return get()[index]; }
+    const T& operator[](size_t index) const noexcept { return get()[index]; }
 
-    Deleter& get_deleter() noexcept {
-        return reinterpret_cast<Deleter&>(storage_);
-    }
-    
-    const Deleter& get_deleter() const noexcept {
-        return reinterpret_cast<const Deleter&>(storage_);
-    }
+    Deleter& get_deleter() noexcept { return std::get<1>(tuple_); }
+    const Deleter& get_deleter() const noexcept { return std::get<1>(tuple_); }
 
     // =================== Modifiers ===============================
     T* release() noexcept {
-        T* tmp = ptr_;
-        ptr_ = nullptr;
+        T* tmp = get();
+        std::get<0>(tuple_) = nullptr;
         return tmp;
     }
     
     void reset(T* ptr = nullptr) noexcept {
-        if (ptr_) {
-            get_deleter()(ptr_);
+        if (get()) {
+            get_deleter()(get());
         }
-        ptr_ = ptr;
+        std::get<0>(tuple_) = ptr;
     }
     
     void swap(UniquePtr& other) noexcept {
-        std::swap(ptr_, other.ptr_);
-        std::swap(get_deleter(), other.get_deleter());
+        std::swap(tuple_, other.tuple_);
     }
 
 private:
-    T* ptr_;
-    mutable typename std::aligned_storage<sizeof(Deleter), alignof(Deleter)>::type storage_;
+    std::tuple<T*, Deleter> tuple_;
 };
 
 // =================== Free function ===============================
